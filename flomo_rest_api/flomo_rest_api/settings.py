cef86fc35efe5704e46e60e1eb5c8aa509377272
Isaac Pure <isaacpure@gmail.com>
2022-09-16 09:42:11 -0500

    JWT backend setup

--- flomo_rest_api/flomo_rest_api/settings.py
+++ flomo_rest_api/flomo_rest_api/settings.py
@@ -90,6 +90,31 @@ DATABASES = {
     }
 }
 
+# Django project settings.py
+
+from datetime import timedelta
+...
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    
+}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
